a39ecf8e2ec54e57edd89c9b56d5214ffe4fbc91
Katlego Dhlamini <katlegodhlamini2003@gmail.com>
2025-10-03 21:04:43 +0200

    Updated javascript

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let isFooterOpen = false;
 
 function toggleFooter() {
+  alert("Hello");
   const footer = document.getElementsByClassName("rectangle")[0];
   const arrow = document.getElementsByClassName("arrowup")[0];
   const footerText = document.getElementById("footerText");
@@ -25,3 +26,32 @@ function toggleFooter() {
   arrow.style.transition = "transform 0.6s ease";
   isFooterOpen = !isFooterOpen;
 }
+function TopFunction() {
+
+  const footer = document.getElementsByClassName("MainBackground")[0];
+  const arrow = document.getElementsByClassName("sliderq")[0];
+  const footerText = document.getElementById("MainBackgroundText");
+
+
+  
+ // Add transitions
+  footer.style.transition = "height 0.6s ease";
+  arrow.style.transition = "transform 0.6s ease";
+
+
+  if (!isFooterOpen) {
+    footer.style.height = "400px";
+    arrow.style.transform = "rotate(180deg)";
+    footerText.style.display = "block"; // Show text
+  } else {
+    footer.style.height = "100px";
+    arrow.style.transform = "rotate(0deg)";
+    footerText.style.display = "none"; // Hide text
+  }
+
+  arrow.style.transition = "transform 0.6s ease";
+  isFooterOpen = !isFooterOpen;
+}
+
+
+

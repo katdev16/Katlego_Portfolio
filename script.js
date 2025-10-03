51499ca9e8b9c95ec49e7652116530ee2f71f37b
Katlego Dhlamini <katlegodhlamini2003@gmail.com>
2025-10-03 07:33:49 +0200

    Add existing file

---
+++ script.js
@@ -0,0 +1,27 @@
+let isFooterOpen = false;
+
+function toggleFooter() {
+  const footer = document.getElementsByClassName("rectangle")[0];
+  const arrow = document.getElementsByClassName("arrowup")[0];
+  const footerText = document.getElementById("footerText");
+
+
+  
+ // Add transitions
+  footer.style.transition = "height 0.6s ease";
+  arrow.style.transition = "transform 0.6s ease";
+
+
+  if (!isFooterOpen) {
+    footer.style.height = "400px";
+    arrow.style.transform = "rotate(180deg)";
+    footerText.style.display = "block"; // Show text
+  } else {
+    footer.style.height = "100px";
+    arrow.style.transform = "rotate(0deg)";
+    footerText.style.display = "none"; // Hide text
+  }
+
+  arrow.style.transition = "transform 0.6s ease";
+  isFooterOpen = !isFooterOpen;
+}

c79cfb0aa213381d0a7d13ae40bdedeafe545e32
Katlego Dhlamini <katlegodhlamini2003@gmail.com>
2025-10-03 21:46:45 +0200

    Updated

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 let isFooterOpen = false;
+function TopFunction() {
+  // alert("Hello");
 
+  const footer = document.getElementsByClassName("MainBackground")[0];
+  const arrow = document.getElementsByClassName("slider1")[0];
+  const footerText = document.getElementById("MainBackgroundText");
+
+
+  
+ // Add transitions
+  footer.style.transition = "height 0.6s ease";
+  arrow.style.transition = "transform 0.6s ease";
+
+
+  if (!isFooterOpen) {
+    footer.style.height = "600px";
+    arrow.style.transform = "rotate(0deg)";
+    footerText.style.display = "block"; // Show text
+  } else {
+    footer.style.height = "50px";
+    arrow.style.transform = "rotate(0deg)";
+    footerText.style.display = "none"; // Hide text
+    arrow.style.height = "20px";
+  }
+
+  arrow.style.transition = "transform 0.6s ease";
+  isFooterOpen = !isFooterOpen;
+}
 function toggleFooter() {
   alert("Hello");
   const footer = document.getElementsByClassName("rectangle")[0];
@@ -26,32 +53,7 @@ function toggleFooter() {
   arrow.style.transition = "transform 0.6s ease";
   isFooterOpen = !isFooterOpen;
 }
-function TopFunction() {
 
-  const footer = document.getElementsByClassName("MainBackground")[0];
-  const arrow = document.getElementsByClassName("sliderq")[0];
-  const footerText = document.getElementById("MainBackgroundText");
-
-
-  
- // Add transitions
-  footer.style.transition = "height 0.6s ease";
-  arrow.style.transition = "transform 0.6s ease";
-
-
-  if (!isFooterOpen) {
-    footer.style.height = "400px";
-    arrow.style.transform = "rotate(180deg)";
-    footerText.style.display = "block"; // Show text
-  } else {
-    footer.style.height = "100px";
-    arrow.style.transform = "rotate(0deg)";
-    footerText.style.display = "none"; // Hide text
-  }
-
-  arrow.style.transition = "transform 0.6s ease";
-  isFooterOpen = !isFooterOpen;
-}
 
 
 

2a777220e785146bb68cd7f7a151debcb1fe5d7a
Katlego Dhlamini <katlegodhlamini2003@gmail.com>
2025-10-14 11:58:06 +0200

    Update on slider

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function TopFunction() {
     arrow.style.transform = "rotate(0deg)";
     footerText.style.display = "block"; // Show text
   } else {
-    footer.style.height = "50px";
+    footer.style.height = "30px";
     arrow.style.transform = "rotate(0deg)";
     footerText.style.display = "none"; // Hide text
     arrow.style.height = "20px";
@@ -28,7 +28,7 @@ function TopFunction() {
   isFooterOpen = !isFooterOpen;
 }
 function toggleFooter() {
-  alert("Hello");
+
   const footer = document.getElementsByClassName("rectangle")[0];
   const arrow = document.getElementsByClassName("arrowup")[0];
   const footerText = document.getElementById("footerText");
@@ -43,15 +43,16 @@ function toggleFooter() {
   if (!isFooterOpen) {
     footer.style.height = "400px";
     arrow.style.transform = "rotate(180deg)";
-    footerText.style.display = "block"; // Show text
+    footerText.style.display = "block"; 
   } else {
-    footer.style.height = "100px";
+    footer.style.height = "30px";
     arrow.style.transform = "rotate(0deg)";
-    footerText.style.display = "none"; // Hide text
+    footerText.style.display = "none"; 
   }
 
   arrow.style.transition = "transform 0.6s ease";
   isFooterOpen = !isFooterOpen;
+  console.log(isFooterOpen);
 }
 
 
